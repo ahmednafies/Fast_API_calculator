4ae00146ecb89f0c0c6f66fd159825051c7e532c
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 18:34:34 +0100

    adds response model and status code

--- api/views.py
+++ api/views.py
@@ -3,6 +3,10 @@ from app.fibonacci import Fibonacci
 from app.ackermann import ackermann
 from app.factorial import factorial
 
+from api.models import FibonacciResponse
+from api.models import AckermannResponse
+from api.models import FactorialResponse
+
 api = APIRouter()
 
 
@@ -11,16 +15,18 @@ def root():
     return {"Hello": "World"}
 
 
-@api.get("/fibonacci/{n}")
+@api.get("/fibonacci/{n}", response_model=FibonacciResponse, status_code=200)
 def fibonacci_view(n: int):
     return {"result": Fibonacci(n).get_sequence()}
 
 
-@api.get("/akermann/{m}&{n}")
+@api.get(
+    "/akermann/{m}&{n}", response_model=AckermannResponse, status_code=200
+)
 def ackermann_view(m: int, n: int):
     return {"result": ackermann(m, n)}
 
 
-@api.get("/factorial/{n}")
+@api.get("/factorial/{n}", response_model=FactorialResponse, status_code=200)
 def factorial_view(n: int):
     return {"result": factorial(n)}
