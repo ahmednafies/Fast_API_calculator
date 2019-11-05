e979e9f28112f54b1d02039cbbbf69a6a6ee3e65
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 22:21:47 +0100

    fixes models

--- api/models.py
+++ api/models.py
@@ -1,14 +1,5 @@
 from pydantic import BaseModel
-from typing import List
 
 
-class FibonacciResponse(BaseModel):
-    result: List[int]
-
-
-class AckermannResponse(BaseModel):
-    result: int
-
-
-class FactorialResponse(BaseModel):
+class Int(BaseModel):
     result: int
