3a0d48709443c0644a119661e2a290c2dff1e74d
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 22:22:31 +0100

    fixes models for api endpoints

--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
-from app.fibonacci import Fibonacci
+from app.fibonacci import fibonacci
 from app.ackermann import ackermann
 from app.factorial import factorial
 
-from api.models import FibonacciResponse
-from api.models import AckermannResponse
-from api.models import FactorialResponse
+from api.models import Int
 
 api = APIRouter()
 
@@ -15,18 +13,16 @@ def root():
     return {"Hello": "World"}
 
 
-@api.get("/fibonacci/{n}", response_model=FibonacciResponse, status_code=200)
+@api.get("/fibonacci/{n}", response_model=Int, status_code=200)
 def fibonacci_view(n: int):
-    return {"result": Fibonacci(n).get_sequence()}
+    return {"result": fibonacci(n)}
 
 
-@api.get(
-    "/akermann/{m}&{n}", response_model=AckermannResponse, status_code=200
-)
+@api.get("/akermann/{m}&{n}", response_model=Int, status_code=200)
 def ackermann_view(m: int, n: int):
     return {"result": ackermann(m, n)}
 
 
-@api.get("/factorial/{n}", response_model=FactorialResponse, status_code=200)
+@api.get("/factorial/{n}", response_model=Int, status_code=200)
 def factorial_view(n: int):
     return {"result": factorial(n)}
