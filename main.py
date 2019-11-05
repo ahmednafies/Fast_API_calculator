88155466d1fdba20f15146f88b12ff94e59a5137
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 23:19:09 +0100

    adds uvicorn for debug purposes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from api import views
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(
     views.api, prefix="/api", responses={404: {"description": "Not found"}}
 )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
