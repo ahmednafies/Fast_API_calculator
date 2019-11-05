62014c1b9fa6a71b7577a98c5f70f19429c483b1
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 23:42:35 +0100

    changes to loops

--- app/factorial.py
+++ app/factorial.py
@@ -1,11 +1,19 @@
 from functools import lru_cache
 
+results = {}
+
 
 @lru_cache(100)
 def factorial(n):
     # TODO: raise negative numbers exceptions
     # TODO: add maximum number validation
     # TODO: Save last computation
+    global results
     if n > 1:
-        return n * factorial(n - 1)
+        index = 1
+        fact = 1
+        while index <= n:
+            fact = fact * index
+            index += 1
+        return fact
     return 1
