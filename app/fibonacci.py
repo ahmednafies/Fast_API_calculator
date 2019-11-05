6827ddb04c263ead79e697b5f1c66f261512d2f4
ahmednafies <ahmed.nafies@gmail.com>
2019-11-05 22:22:55 +0100

    optimizes the recursive function

--- app/fibonacci.py
+++ app/fibonacci.py
@@ -1,22 +1,30 @@
 from functools import lru_cache
 
+results = []
 
-class Fibonacci:
 
-    previous_results = {}
+def compute(n) -> int:
+    if n <= 1:
+        return n
+    return compute(n - 1) + compute(n - 2)
 
-    def __init__(self, n):
-        self.n = n
 
-    def _compute(self, n) -> int:
-        if n <= 1:
-            return n
-        return self._compute(n - 1) + self._compute(n - 2)
+@lru_cache(100)
+def fibonacci(n) -> int:
+    # TODO: raise negative numbers exceptions
+    # TODO: add maximum number validation
+    # TODO: save last computation
+    n += 1
+    global results
+    if not results:
+        results = [compute(i) for i in range(n)]
+        return results[-1]
 
-    @lru_cache(100)
-    def get_sequence(self) -> list:
-        # TODO: raise negative numbers exceptions
-        # TODO: add maximum number validation
-        # TODO: save last computation
+    if n < len(results):
+        return results[n - 1]
 
-        return [self._compute(i) for i in range(self.n)]
+    if n > len(results):
+        results = results + [compute(i) for i in range(len(results), n)]
+        return results[-1]
+
+    return results[-1]
