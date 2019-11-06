2dbe808db9376d3ea89a2dcce9a642d52f01d77b
ahmednafies <ahmed.nafies@gmail.com>
2019-11-06 16:12:00 +0100

    optimises factorial

--- app/factorial.py
+++ app/factorial.py
@@ -1,19 +1,40 @@
 from functools import lru_cache
 
-results = {}
+results = []
 
 
-@lru_cache(100)
+def compute_first(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+        results.append(num)
+
+
+def accumulate(n):
+    num = results[-1]
+    last_index = results.index(results[-1])
+    while last_index < n:
+        num = num * n
+        n = n + 1
+        results.append(num)
+
+
+# @lru_cache(100)
 def factorial(n):
     # TODO: raise negative numbers exceptions
     # TODO: add maximum number validation
     # TODO: Save last computation
-    global results
+    if n < 1:
+        return 1
+
     if n > 1:
-        index = 1
-        fact = 1
-        while index <= n:
-            fact = fact * index
-            index += 1
-        return fact
-    return 1
+        compute_first(n)
+        return results[-1]
+
+    if n < len(results):
+        return results[n]
+
+    if n > len(results):
+        accumulate(n)
+        return results[-1]
