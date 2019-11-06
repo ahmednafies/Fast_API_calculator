d75bf5d73f7066c2f692cc6fe6677504ebd85430
ahmednafies <ahmed.nafies@gmail.com>
2019-11-06 22:25:44 +0100

    fixes status code

--- app/fibonacci.py
+++ app/fibonacci.py
@@ -1,36 +1,35 @@
 from functools import lru_cache
 
-results = []
+results = [0, 1]
 
 
-def compute(n) -> int:
-    if n <= 1:
-        return n
-    return compute(n - 1) + compute(n - 2)
-
-
-def compute_next(diff, sequence) -> int:
-    for index in range(diff):
-        sequence.append(results[-1] + results[-2])
-    return sequence
+def compute_next(start, end) -> int:
+    count = start
+    while count < end:
+        results.append(results[-2] + results[-1])
+        count += 1
 
 
 def fibonacci(n) -> int:
     # TODO: raise negative numbers exceptions
     # TODO: add maximum number validation
     # TODO: save last computation
-    n += 1
-    global results
-    if not results:
-        results = [compute(i) for i in range(n)]
+    if n < 0:
+        print("Please enter a positive integer")
+
+    if n == 0 or n == 1:
+        return n
+
+    if len(results) == 2:
+        n = n - 1  # because we already have values in the results
+        compute_next(0, n)
         return results[-1]
 
     if n < len(results):
-        return results[n - 1]
+        return results[n]
 
     if n > len(results):
-        diff = n - len(results)
-        results = results + compute_next(diff, results)
+        compute_next(len(results), n + 1)
         return results[-1]
 
     return results[-1]
