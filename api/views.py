5694c71f9f889dd36624658849201e44a447e6d9
ahmednafies <ahmed.nafies@gmail.com>
2019-11-07 17:15:42 +0100

    adds time

--- api/views.py
+++ api/views.py
@@ -11,7 +11,8 @@ api = APIRouter()
 @api.get("/fibonacci/{n}", response_model=Int, status_code=200)
 def fibonacci_view(n: int):
     if fibonacci_valid_input(n):
-        return {"result": fibonacci(n)}
+        result, time = fibonacci(n)
+        return {"result": result, "time": time}
 
 
 @api.get("/akermann/{m}&{n}", response_model=Int, status_code=200)
@@ -22,4 +23,5 @@ def ackermann_view(m: int, n: int):
 @api.get("/factorial/{n}", response_model=Int, status_code=200)
 def factorial_view(n: int):
     if factorial_valid_input(n):
-        return {"result": factorial(n)}
+        result, time = factorial(n)
+        return {"result": result, "time": time}
