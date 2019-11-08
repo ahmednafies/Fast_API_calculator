3ad6fe38a2e30ab41577b2072974229747125b36
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 13:18:50 +0100

    adds test for factorial

---
+++ app/tests/test_factorial.py
@@ -0,0 +1,25 @@
+from app.factorial import factorial
+from app.exceptions import ValidationError
+import pytest
+import config
+import math
+
+factorial_sequence = [math.factorial(num) for num in range(10)]
+
+
+def test_factorial_raises_exception_with_invalid_values():
+    with pytest.raises(ValidationError):
+        factorial(config.FACTORIAL_MIN_VALUE - 1)
+
+    with pytest.raises(ValidationError):
+        factorial(config.FACTORIAL_MAX_VALUE + 1)
+
+    with pytest.raises(TypeError):
+        factorial("string")
+
+
+def test_factorial_with_valid_data():
+    for index, value in enumerate(factorial_sequence):
+        fact, _ = factorial(index)
+        assert fact == value
+
