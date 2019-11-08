ad91a32779830c94af5bfd9f10610f1b5d19f479
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 18:00:46 +0100

    fixes status code

--- api/validators.py
+++ api/validators.py
@@ -4,19 +4,19 @@ from fastapi import HTTPException
 def is_valid_input(name, val, min_val, max_val):
     if not isinstance(val, int):
         raise HTTPException(
-            status_code=400,
+            status_code=422,
             detail=f"input must be a positive integer for '{name}'",
         )
 
     if val < min_val:
         raise HTTPException(
-            status_code=400,
+            status_code=422,
             detail=f"negative numbers are not allowed for '{name}'",
         )
 
     if val > max_val:
         raise HTTPException(
-            status_code=400,
+            status_code=422,
             detail=f"max number allowed for '{name}' is ({max_val})",
         )
 
