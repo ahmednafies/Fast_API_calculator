d55ff3c6f69fc78bd9b772f251a3759aab12f61b
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 15:29:46 +0100

    fixes ackermann time evaluation

--- app/ackermann.py
+++ app/ackermann.py
@@ -1,18 +1,22 @@
 import sys
 from functools import lru_cache
+from app.utils import eval_time
 
 sys.setrecursionlimit(10000)
 
 
-@lru_cache(100)
-def ackermann(m, n):
-    # TODO: raise negative numbers exceptions
-    # TODO: max number
-    # Save last computation
+def compute(m, n):
     if m == 0:
         return n + 1
     if m > 0 and n == 0:
-        return ackermann(m - 1, 1)
+        return compute(m - 1, 1)
 
     if m > 0 and n > 0:
-        return ackermann(m - 1, ackermann(m, n - 1))
+        return compute(m - 1, compute(m, n - 1))
+
+
+@eval_time
+@lru_cache(100)
+def ackermann(m, n):
+    result = compute(m, n)
+    return result
