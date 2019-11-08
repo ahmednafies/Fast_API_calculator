c82bf162ad3bac1810a9290230e5f9e338035a91
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 15:27:51 +0100

    modifies models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,55 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from api.validators import is_valid_input
+import config
 
 
-class Int(BaseModel):
+class Response(BaseModel):
     result: int
     time: str
+
+
+class FibonacciModel(BaseModel):
+    n: int
+
+    @validator("n")
+    def n_is_valid(cls, value):
+        is_valid_input(
+            "n", value, config.FIBONACCI_MIN_VALUE, config.FIBONACCI_MAX_VALUE
+        )
+        return value
+
+
+class AckermannModel(BaseModel):
+    m: int
+    n: int
+
+    @validator("m")
+    def m_is_valid(cls, value):
+        is_valid_input(
+            "m",
+            value,
+            config.ACKERMANN_MIN_VALUE[0],
+            config.ACKERMANN_MAX_VALUE[0],
+        )
+        return value
+
+    @validator("n")
+    def n_is_valid(cls, value):
+        is_valid_input(
+            "n",
+            value,
+            config.ACKERMANN_MIN_VALUE[1],
+            config.ACKERMANN_MAX_VALUE[1],
+        )
+        return value
+
+
+class FactorialModel(BaseModel):
+    n: int
+
+    @validator("n")
+    def n_is_valid(cls, value):
+        is_valid_input(
+            "n", value, config.FACTORIAL_MIN_VALUE, config.FACTORIAL_MAX_VALUE
+        )
+        return value
