c4accc304375b01f92a7be788fc37e0d5f25ce55
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 15:28:55 +0100

    fixes views

--- api/views.py
+++ api/views.py
@@ -2,26 +2,24 @@ from fastapi import APIRouter
 from app.fibonacci import fibonacci
 from app.ackermann import ackermann
 from app.factorial import factorial
-from api.validators import factorial_valid_input, fibonacci_valid_input
-from api.models import Int
+from api.models import Response, FactorialModel, AckermannModel, FibonacciModel
 
 api = APIRouter()
 
 
-@api.get("/fibonacci/{n}", response_model=Int, status_code=200)
-def fibonacci_view(n: int):
-    if fibonacci_valid_input(n):
-        result, time = fibonacci(n)
-        return {"result": result, "time": time}
+@api.post("/fibonacci/", response_model=Response, status_code=200)
+def fibonacci_view(model: FibonacciModel):
+    result, time = fibonacci(model.n)
+    return {"result": result, "time": time}
 
 
-@api.get("/akermann/{m}&{n}", response_model=Int, status_code=200)
-def ackermann_view(m: int, n: int):
-    return {"result": ackermann(m, n)}
+@api.post("/akermann/", response_model=Response, status_code=200)
+def ackermann_view(model: AckermannModel):
+    result, time = ackermann(model.m, model.n)
+    return {"result": result, "time": time}
 
 
-@api.get("/factorial/{n}", response_model=Int, status_code=200)
-def factorial_view(n: int):
-    if factorial_valid_input(n):
-        result, time = factorial(n)
-        return {"result": result, "time": time}
+@api.post("/factorial", response_model=Response, status_code=200)
+def factorial_view(model: FactorialModel):
+    result, time = factorial(model.n)
+    return {"result": result, "time": time}
