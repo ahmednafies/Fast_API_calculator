611c0dda2e7f9861110bf52c5c638ca4ae6f111d
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 12:20:01 +0100

    factorial validator decorator

--- app/factorial.py
+++ app/factorial.py
@@ -1,46 +1,32 @@
 from functools import lru_cache
-from app.validators import is_valid_number
-from app.utils import eval_time
+from app.utils import eval_time, validate
 import config
 
-results = {}
+results = {0: 1, 1: 1}
 last_num = 1
 
 
-def compute(fact, next_num, num):
+def compute_next(num):
     global last_num
 
-    while next_num <= num:
-        fact *= next_num
+    while last_num < num:
+        next_num = last_num + 1
+        fact = next_num * results[last_num]
         results[next_num] = fact
-        next_num += 1
-
-    last_num = next_num - 1
-    return results[last_num]
+        last_num += 1
 
 
 @eval_time
-@lru_cache(1000)
+@lru_cache(500)
+@validate(
+    min_val=config.FACTORIAL_MIN_VALUE, max_val=config.FACTORIAL_MAX_VALUE
+)
 def factorial(num):
     global last_num
-
-    is_valid_number(
-        num, config.FACTORIAL_MIN_VALUE, config.FACTORIAL_MAX_VALUE
-    )
-
-    if num == 0:
-        return 1
-
-    if not results:
-        return compute(1, 1, num)
-
     if num < last_num:
         return results[num]
 
-    elif num > last_num:
-        fact = results[last_num]
-        next_num = last_num + 1
-        return compute(fact, next_num, num)
+    if num > last_num:
+        compute_next(num)
 
-    else:
-        return results[last_num]
+    return results[last_num]
