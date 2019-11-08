17f942995eadd601dac00461c82d0ac6fa97db05
ahmednafies <ahmed.nafies@gmail.com>
2019-11-08 18:01:33 +0100

    adds endpoint tests

---
+++ api/tests/test_factorial_endpoint.py
@@ -0,0 +1,26 @@
+from starlette.testclient import TestClient
+from api.views import api
+from fastapi.exceptions import HTTPException, RequestValidationError
+import pytest
+import config
+
+client = TestClient(api)
+
+endpoint = "/factorial/"
+
+
+def test_factorial_with_valid_data():
+    response = client.post(endpoint, json={"n": 5})
+    assert response.status_code == 200
+    assert response.json()["result"] == 120
+
+
+def test_factorial_with_invalid_data():
+    with pytest.raises(HTTPException):
+        client.post(endpoint, json={"n": config.FACTORIAL_MIN_VALUE - 1})
+
+    with pytest.raises(HTTPException):
+        client.post(endpoint, json={"n": config.FACTORIAL_MAX_VALUE + 1})
+
+    with pytest.raises(RequestValidationError):
+        client.post(endpoint, json={"n": "hello"})
---
+++ api/tests/test_fibonacci_endpoint.py
@@ -0,0 +1,24 @@
+from starlette.testclient import TestClient
+from api.views import api
+from fastapi.exceptions import HTTPException, RequestValidationError
+import pytest
+import config
+
+client = TestClient(api)
+
+
+def test_fibonacci_with_valid_data():
+    response = client.post("/fibonacci/", json={"n": 10})
+    assert response.status_code == 200
+    assert response.json()["result"] == 55
+
+
+def test_fibonacci_with_invalid_data():
+    with pytest.raises(HTTPException):
+        client.post("/fibonacci/", json={"n": config.FIBONACCI_MIN_VALUE - 1})
+
+    with pytest.raises(HTTPException):
+        client.post("/fibonacci/", json={"n": config.FIBONACCI_MAX_VALUE + 1})
+
+    with pytest.raises(RequestValidationError):
+        client.post("/fibonacci/", json={"n": "hello"})
