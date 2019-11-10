b9e7fd3f92500972cf85756b052578eaf85eed24
ahmednafies <ahmed.nafies@gmail.com>
2019-11-10 15:31:42 +0100

    optimises ackermann

--- app/ackermann.py
+++ app/ackermann.py
@@ -1,5 +1,8 @@
 from functools import lru_cache
 from app.utils import eval_time
+import config
+from app.utils import validate
+from app.validators import is_valid_number
 
 
 def knuth_to_power(n):
@@ -27,11 +30,15 @@ def compute(m, n):
         return (2 ** knuth_to_power(n)) - 3
 
     if m == 5:
-        return (2 ** knuth_to_power(0)) - 3
+        return (2 ** knuth_to_power(1)) - 3
 
 
 @eval_time
 @lru_cache(100)
+@validate(config.ACKERMANN_M_MIN, config.ACKERMANN_M_MAX)
 def ackermann(m, n):
+    n_min_val = config.ACKERMANN_LIMITS["m"][m]["n"]["min"]
+    n_max_val = config.ACKERMANN_LIMITS["m"][m]["n"]["max"]
+    is_valid_number(n, n_min_val, n_max_val)
     result = compute(m, n)
     return result
