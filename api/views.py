6cf6e8c3bd0f3b4972ac72e03756ef832fae76ff
ahmednafies <ahmed.nafies@gmail.com>
2019-11-10 16:29:56 +0100

    fixes views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from app.fibonacci import fibonacci
+from fastapi import APIRouter
+
+from api.models import AckermannModel, FactorialModel, FibonacciModel, Response
 from app.ackermann import ackermann
 from app.factorial import factorial
-from api.models import Response, FactorialModel, AckermannModel, FibonacciModel
-from fastapi import APIRouter
+from app.fibonacci import fibonacci
 
 api = APIRouter()
 
@@ -15,7 +16,7 @@ def fibonacci_view(model: FibonacciModel):
 
 @api.post("/akermann/", response_model=Response, status_code=200)
 def ackermann_view(model: AckermannModel):
-    result, time = ackermann(model.m, model.n)
+    result, time = ackermann(model.values.m, model.values.n)
     return {"result": result, "time": time}
 
 
